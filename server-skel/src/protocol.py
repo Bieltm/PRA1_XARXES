4e09d6ab3e8afee2292a935905a554329f2e5bf0
Bieltm <bieltorruella2006@gmail.com>
2026-04-11 20:36:30 +0200

    finished with protocol.py

--- server-skel/src/protocol.py
+++ server-skel/src/protocol.py
@@ -1,6 +1,8 @@
 #Definiu l’enum Opcode (valors a RFC-31337 §5) i la classe VpnHeader (format a RFC-31337 §4.1).
 import struct
 from enum import IntEnum
+from enum import Enum
+
 
 class Opcode(IntEnum):
     REGISTER  = 0x01
@@ -29,20 +31,36 @@ class VpnHeader:
         except ValueError:
             return None
         
-    def Encode_NumSeq(numero):
-        return struct.pack("!Q", numero)
-    def Decode_NumSeq(payload):
-        return struct.unpack("!Q", payload)[0]
-    #TODO:
-    #Extraccio de les adreçes MAC
-    def Get_MadAddr(trama_ethernet):
-        if len(trama_ethernet) < 14:
-            return None, None
-        dest_mac = trama_ethernet[0:6] 
-        src_mac = trama_ethernet[6:12] 
-        return dest_mac, src_mac
-    #Format de MAC per a humans
-    #Gestió de la contrasenya
-    #Definició de la politica de MACs desconegudes
-    #Verificació de la mida de la trama ETHERNET
-    
+def Encode_NumSeq(numero):
+    return struct.pack("!Q", numero)
+def Decode_NumSeq(payload):
+    return struct.unpack("!Q", payload)[0]
+#Extraccio de les adreçes MAC
+def Get_MadAddr(trama_ethernet):
+    if len(trama_ethernet) < 14:
+        return None, None
+    dest_mac = trama_ethernet[0:6] 
+    src_mac = trama_ethernet[6:12] 
+    return dest_mac, src_mac
+#Format de MAC per a humans (convertir a hexadecimal)
+def Convert_Hex(mac):
+    res = "" 
+    for i in range(0, len(mac)):
+        v_hex = f"{mac[i]:02x}"
+        if i > 0:
+            res += ":"
+        res += v_hex
+    return res.upper()
+#Gestió de la contrasenya
+def Convert_Password(VpnHeader):
+    if (VpnHeader.opcode!=0x02):
+        return ""
+    return VpnHeader.payload.decode('ascii').strip('\x00')
+#Definició de la politica de MACs desconegudes
+class UnknownMacPolicy(Enum):
+    FLOOD = "flood"      # Inundar tots els clients
+    DISCARD = "discard"  # Descartar la trama
+#Verificació de la mida de la trama ETHERNET
+def is_valid_frame(trama_ethernet):
+    # Comprova si la longitud és de 14 o més
+    return len(trama_ethernet) >= 14
